7cc95d9df9867071511862253b088250f85a15d1
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2024-04-23 11:34:21 +0300

    Замена строки

--- queries.sql
+++ queries.sql
@@ -2,3 +2,12 @@ DELETE
 FROM resumes_table
 WHERE
   resume_id = <resume_id>;
+
+REPLACE INTO token_table (id, access_token, refresh_token, expires)
+VALUES
+  (
+  1,
+  "access_token1",
+  "refresh_token1",
+  CAST("1713859589.3007352" AS Timestamp)
+  );
