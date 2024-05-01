3d28d1fdce8a90fbce740fc2f820d92b5a2ef713
Sergey Vakhnin <sergey.vakhnin@gmail.com>
2024-05-01 11:23:51 +0300

    Поднятие резюме из списка

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ import time
 import requests
 import ydb
 
-from db import driver_config, select_tokens, set_tokens
+from db import driver_config, select_tokens, set_tokens, select_resumes
 from hh_logging import logger
 
 BASE_URL = 'https://api.hh.ru'
@@ -51,14 +51,32 @@ def main(event, context):
 
                 tokens = select_tokens(session)
                 headers['Authorization'] = 'Bearer ' + tokens['access_token']
+                logger.debug('Токены обновлены', extra={'token-expiries': tokens['expires']})
             elif response.status_code != 200:
                 logger.error(f'Неизвестная ошибка с кодом {response.status_code}')
                 logger.error(response.text)
                 exit(1)
-            response = requests.get(f'{BASE_URL}/resumes/mine', headers=headers)
-            print(f'Answer code {response.status_code}')
 
-            logger.info("My log message", extra={"my-key": "my-value"})
+            result = []
+            resumes = select_resumes(session)
+            for resume in resumes:
+                response = requests.post(f'{BASE_URL}/resumes/{resume["resume_id"]}/publish',
+                                         headers=headers)
+                result.append(
+                    {
+                        'resume_name': resume["name"],
+                        'resume_status': response.status_code,
+                    })
+
+            all_ups_ok = True
+            for i in range(len(result)):
+                if result[i]['resume_status'] != 204:
+                    all_ups_ok = False
+                result[i] = f"{result[i]['resume_name']}: {result[i]['resume_status']}"
+            if all_ups_ok:
+                logger.info(', '.join(result), extra={'token-expiries': tokens['expires']})
+            else:
+                logger.error(', '.join(result), extra={'token-expiries': tokens['expires']})
         except TimeoutError:
             print("Connect failed to YDB")
             print("Last reported errors by discovery:")
